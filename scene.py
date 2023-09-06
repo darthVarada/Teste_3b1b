9dfde5d17a7e707762a8de47c53fa36e07662178
darthVarada <fafapelegia@hotmail.com>
2023-09-06 00:25:00 -0300

    Mensagem de commit descrevendo suas mudanças

--- scene.py
+++ scene.py
@@ -1,26 +1,8 @@
-from manim import *
-
-class MineScopeLogo(Scene):
+from manimlib.imports import *
+class BarCharExam(Scene):
     def construct(self):
-        self.camera.background_color = "#ece6e2"
-        logo_green = "#87c2a5"
-        logo_blue = "#525893"
-        logo_red = "#e07a5f"
-        logo_black = "#343434"
-        
-        # Customized text for MineScope
-        mine_scope = Text("MineScope", color=logo_black, font_size=72)
-        
-        circle = Circle(color=logo_green, fill_opacity=1).shift(LEFT)
-        square = Square(color=logo_blue, fill_opacity=1).shift(UP)
-        triangle = Triangle(color=logo_red, fill_opacity=1).shift(RIGHT)
-        
-        logo = VGroup(triangle, square, circle, mine_scope)  # order matters
-        logo.arrange(RIGHT, buff=1.0)
-        logo.move_to(ORIGIN)
-        
-        self.add(logo)
-
-# Use one of the following lines to render the animation
-# MineScopeLogo("render_version").render()  # Use "rl" for rapid render
-MineScopeLogo("preview_version").preview()  # Use "pl" for rapid preview
+        colors = [RED, GREEN, BLUE, YELLOW, ORANGE]
+        values = [0.5, 0.6, 0.9, 0.45, 0.96, 0.73, 0.2, 0.4, 0.49, 0.75, 0.9]
+        bar_chart = BarChart(values, bar_colors = colors)
+        self.play(DrawBorderThenFill(bar_chart, run_time = 5))
+        self.wait(3)
